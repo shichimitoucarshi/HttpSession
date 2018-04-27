553d38987205df077c82638d7c61a40a31cc2cf5
keisuke <jam330157@gmail.com>
2018-04-28 02:46:56 +0900

    add app info

--- HttpSession/HttpSession/Request.swift
+++ HttpSession/HttpSession/Request.swift
@@ -31,10 +31,63 @@ open class Request {
         self.urlReq.httpMethod = method.rawValue
         self.urlReq.httpShouldHandleCookies = false
         self.urlReq.timeoutInterval = 60
+        self.urlReq.allHTTPHeaderFields = Request.appInfo
         if cookie == true {
             self.urlReq.allHTTPHeaderFields = Cookie.shared.get(url: url)
         }
     }
+    
+    open static let appInfo:[String: String] = {
+        
+        let acceptEncoding: String = "gzip;q=1.0, compress;q=0.5"
+        
+        // Accept-Language HTTP Header; see https://tools.ietf.org/html/rfc7231#section-5.3.5
+        let acceptLang = Locale.preferredLanguages.prefix(6).enumerated().map { index, languageCode in
+            let quality = 1.0 - (Double(index) * 0.1)
+            return "\(languageCode);q=\(quality)"
+            }.joined(separator: ", ")
+        
+        // User-Agent Header; see https://tools.ietf.org/html/rfc7231#section-5.5.3
+        let userAgent: String = {
+            if let info = Bundle.main.infoDictionary {
+                let executable = info[kCFBundleExecutableKey as String] as? String ?? "Unknown"
+                let bundle = info[kCFBundleIdentifierKey as String] as? String ?? "Unknown"
+                let appVersion = info["CFBundleShortVersionString"] as? String ?? "Unknown"
+                let appBuild = info[kCFBundleVersionKey as String] as? String ?? "Unknown"
+                
+                let version: String = {
+                    let sysVer = ProcessInfo.processInfo.operatingSystemVersion
+                    let version = "\(sysVer.majorVersion).\(sysVer.minorVersion).\(sysVer.patchVersion)"
+                    
+                    let os: String = {
+                        #if os(iOS)
+                            return "iOS"
+                        #elseif os(watchOS)
+                            return "watchOS"
+                        #elseif os(tvOS)
+                            return "tvOS"
+                        #elseif os(macOS)
+                            return "OS X"
+                        #elseif os(Linux)
+                            return "Linux"
+                        #else
+                            return "Unknown"
+                        #endif
+                    }()
+                    
+                    return "\(os)/\(version)"
+                }()
+                return "\(executable)/\(appVersion) (\(bundle)) \(version) buildver \(appBuild)"
+            }
+            return "HttpSession"
+        }()
+        
+        return [
+            "Accept-Encoding": acceptEncoding,
+            "Accept-Language": acceptLang,
+            "User-Agent": userAgent
+        ]
+    }()
     
     public func  postHttp (param: Dictionary<String, String>) -> URLRequest{
         self.urlReq.setValue("application/json", forHTTPHeaderField: "Content-Type")
