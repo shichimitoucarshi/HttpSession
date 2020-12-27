79c3c010a158565fca160eb03859c66b8477e6b3
keisukeYamagishi <jam330257@gmail.com>
2020-12-27 11:01:42 +0900

    add test code

---
+++ HttpSessionTests/HttpSessionTests.swift
@@ -0,0 +1,119 @@
+//
+//  HttpSessionTests.swift
+//  HttpSessionTests
+//
+//  Created by Shichimitoucarashi on 12/1/18.
+//  Copyright © 2018 keisuke yamagishi. All rights reserved.
+//
+
+import HttpSession
+import XCTest
+
+class HttpSessionTests: XCTestCase {
+    func testHttpSession() {
+        let exp = expectation(description: "Single Exception")
+        Http.request(url: "https://httpsession.work/getApi.json", method: .get)
+            .session { data, _, _ in
+                XCTAssertNotNil(data)
+                exp.fulfill()
+            }
+        wait(for: [exp], timeout: 60.0)
+    }
+
+    func testHttpPost() {
+        let exp = expectation(description: "Single Exception")
+        let param = ["http_post": "Http Request POST 😄"]
+        Http.request(url: "https://httpsession.work/postApi.json",
+                     method: .post,
+                     params: param).session { data, _, _ in
+                        XCTAssertNotNil(data)
+                        exp.fulfill()
+                     }
+        wait(for: [exp], timeout: 60.0)
+    }
+
+    func testHttpCookieSignIn() {
+        let exp = expectation(description: "Single Exception")
+        let param1 = ["http_sign_in": "Http Request SignIn",
+                      "userId": "keisukeYamagishi",
+                      "password": "password_jisjdhsnjfbns"]
+        let url = "https://httpsession.work/signIn.json"
+
+        Http.request(url: url,
+                     method: .post,
+                     params: param1).session { data, _, _ in
+                        XCTAssertNotNil(data)
+                        exp.fulfill()
+                     }
+        wait(for: [exp], timeout: 60.0)
+    }
+
+    func testHttpCookieSignIned() {
+        let exp = expectation(description: "Single Exception")
+        Http.request(url: "https://httpsession.work/signIned.json",
+                     method: .get,
+                     cookie: true).session { data, _, _ in
+                        XCTAssertNotNil(data)
+                        exp.fulfill()
+                     }
+        wait(for: [exp], timeout: 60.0)
+    }
+
+    func testHttpMultipart() {
+        let exp = expectation(description: "Single Exception")
+        let multipartData = Multipart()
+        let image: String? = Bundle.main.path(forResource: "re", ofType: "txt")
+        var img: Data = Data()
+        do {
+            img = try Data(contentsOf: URL(fileURLWithPath: image!))
+        } catch {}
+
+        multipartData.fileName = "Hello.txt"
+        multipartData.mimeType = "text/plain"
+        multipartData.data = img
+
+        Http.request(url: "https://httpsession.work/imageUp.json",
+                     method: .post, multipart: ["img": multipartData])
+            .upload(completionHandler: { data, _, _ in
+                XCTAssertNotNil(data)
+                exp.fulfill()
+            })
+        wait(for: [exp], timeout: 60.0)
+    }
+
+    func testHttpBasicAuth() {
+        let exp = expectation(description: "Single Exception")
+        let basicAuth: [String: String] = [Auth.user: "httpSession",
+                                           Auth.password: "githubHttpsession"]
+        Http.request(url: "https://httpsession.work/basicauth.json",
+                     method: .get,
+                     basic: basicAuth).session { data, _, _ in
+                        XCTAssertNotNil(data)
+                        exp.fulfill()
+                     }
+        wait(for: [exp], timeout: 60.0)
+    }
+
+    func testHttpDownload() {
+        let exp = expectation(description: "Download Exception")
+        Http.request(url: "https://shichimitoucarashi.com/public/Apple_trim.mp4", method: .get).download(progress: { _, total, expectedToWrite in
+            let progress = Float(total) / Float(expectedToWrite)
+            print("progress \(progress)")
+        }, download: { url in
+            print("URL: \(String(describing: url))")
+            XCTAssertNotNil(url)
+        }) { _, _, _ in
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 60.0)
+    }
+
+    func testApiProtoccol_Upload() {
+        let exp = expectation(description: "Download Exception")
+        ApiProvider<TestApi>().upload(api: .test3) { data, _, _ in
+            XCTAssertNotNil(data)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 60.0)
+    }
+}
